4fd88f55ebe1fa209f4ab2a8084419e88ed70ebb
caue-ferreira-nave <caue.ferreira@nave.org.br>
2026-06-05 11:05:15 -0300

    1dia

--- src/AtvMetodoFacil.js
+++ src/AtvMetodoFacil.js
@@ -20,3 +20,37 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+const personagem ={
+    nome: "romeu romulo",
+    classe: "bardo",
+    nivel: 12,
+    equipamentos: ["arco", "violão", "gaita", "flechas de aço",
+                   "botas de couro", "capa de couro", "chapéu de pena" ],
+     pontosDeVida: 100, 
+     pontosDeMagia: 100,
+
+  moedas: {
+    ouro: 20,
+    prata: 43,
+    bronze: 125
+  },
+  pocoes: {
+    cura: 7,
+    magia: 5
+  },
+   tocarMusica(musica, letra) {
+    console.log(`
+Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+uma música para trazer ânimo a seus corpos e esperança para seus corações.
+Apresento-lhes a música ${musica}.
+
+Em 1, 2, 3... ${letra}  `);
+  }
+};
+personagem.tocarMusica(
+  "misery business",
+  "Im in the business of misery lets take it from the top Shes got a body like an hourglass its ticking like a clock Its a matter of time before we all run out When I thought he was mine she caught him by the mouth")
+
+  console.log(personagem.tocarMusica)
+
+  export default personagem;
