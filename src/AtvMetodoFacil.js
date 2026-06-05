5aec3fd9c2b315f7b0c5fd5adf1baa7851479b49
Ryan-Nave2026 <ryan.nave2026@gmail.com>
2026-06-05 10:43:59 -0300

    fix

--- src/AtvMetodoFacil.js
+++ src/AtvMetodoFacil.js
@@ -20,3 +20,34 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+ const Personagem = {
+    nome: 'Romeu Rômulo',
+    nivel: 12,
+    classe: 'bardo',
+    equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro", "capa de couro", "chapéu de pena"],
+    pontosDeVida: 100,
+    pontosDeMagia: 100,
+    moedas: {
+        ouro: 20,
+        prata: 43,
+        bronze: 125
+    }, 
+    pocoes: {
+        cura: 7,
+        magia: 5
+    },
+    tocarMusica: function(nome, letra){
+        console.log(
+        `Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+        uma música para trazer ânimo a seus corpos e esperança para seus corações.
+        Apresento-lhes a música [${nome}].
+
+        Em 1, 2, 3
+
+        ${letra}`) 
+    }
+  }
+
+console.log(copiaDoPersonagem)
+copiaDoPersonagem.tocarMusica('Blue Bird', 'hishou taitara\n modoranai to itte\n mezashita no wa\n aoi aoi ano sora\n kanashimi wa mada oboerarezu\n setsunasa wa ima tsukami hajimetaanata\n e to daku kono kanjou mo\n ima kotoba ni kawatte iku\n michi naru sekai no\n yuumei kara mezamete\n kono hane wo hiroge tobitatsu\n hishou taitara\n modoranai to itte\n mezashita no wa\n shiroi shiroi ano kumo\n tsukinuketara\n mitsukaru to shitte\n furikiru hodo\n aoi aoi ano sora\n aoi aoi ano sora\n aoi aoi ano sora')
+export default Personagem;
