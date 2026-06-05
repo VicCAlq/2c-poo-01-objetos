7400b0cd57eace361849273d95b09ce48ab75973
Isaac-Andrade-Santos-NAVE <isaac.andrade@nave.org.br>
2026-06-05 14:04:47 +0000

    Alterados os arquivos AtvObjeto, AtvMetodoFacil, AtvMetodoMedio e AtvMetodoComplicado (Atividade concluída).

--- src/AtvMetodoComplicado.js
+++ src/AtvMetodoComplicado.js
@@ -39,3 +39,62 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+const personagem = {
+	nome: "Romeu Rômulo",
+	classe: "bardo",
+	nivel: 12,
+	equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro", "capa de couro", "chapéu de pena"],
+	pontosDeVida: 30,
+	pontosDeMagia: 100,
+	moedas: {
+		ouro: 20,
+		prata: 43,
+		bronze: 125
+	},
+	pocoes: {
+		cura: 0,
+		magia: 5
+	},
+	tocarMusica: function(musica, letra) {
+		console.log(`
+	Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+	uma música para trazer ânimo a seus corpos e esperança para seus corações.
+	Apresento-lhes a música ${musica}.
+
+	Em 1, 2, 3
+	...
+	${letra}`)
+	},
+	consumirPocaoCura: function() {
+		if (this.pocoes.cura == 0)
+			return console.log("Não é possível se curar sem poções de cura!")
+
+		this.pocoes.cura--
+		this.pontosDeVida = 100
+	},
+	comprarPocoesCura: function(quantidade) {
+		const precoPocao = 15
+		let custoTotal = precoPocao * quantidade
+		let conseguiu = false
+
+		if (custoTotal <= this.moedas.prata) {
+			this.moedas.prata -= custoTotal
+			conseguiu = true
+		} else if (10 * custoTotal <= this.moedas.bronze) {
+			this.moedas.bronze -= custoTotal * 10
+			conseguiu = true
+		} else if (parseInt(custoTotal / 10) <= this.moedas.ouro) {
+			this.moedas.ouro -= parseInt(parseInt(custoTotal / 10))
+			conseguiu = true
+		}
+
+		if (!conseguiu) {
+			console.log("Você não tem dinheiro o suficiente, meu caro. Quer fazer um empréstimo?")
+			return
+		}
+
+		this.pocoes.cura += quantidade
+	}
+}
+
+export default personagem
--- src/AtvMetodoMedio.js
+++ src/AtvMetodoMedio.js
@@ -16,3 +16,39 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+const personagem = {
+	nome: "Romeu Rômulo",
+	classe: "bardo",
+	nivel: 12,
+	equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro", "capa de couro", "chapéu de pena"],
+	pontosDeVida: 30,
+	pontosDeMagia: 100,
+	moedas: {
+		ouro: 20,
+		prata: 43,
+		bronze: 125
+	},
+	pocoes: {
+		cura: 7,
+		magia: 5
+	},
+	tocarMusica: function(musica, letra) {
+		console.log(`
+	Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+	uma música para trazer ânimo a seus corpos e esperança para seus corações.
+	Apresento-lhes a música ${musica}.
+
+	Em 1, 2, 3
+	...
+	${letra}`)
+	},
+	consumirPocaoCura: function() {
+		if (this.pocoes.cura == 0)
+			return console.log("Não é possível se curar sem poções de cura!")
+
+		this.pocoes.cura--
+		this.pontosDeVida = 100
+	}
+}
+
+export default personagem
