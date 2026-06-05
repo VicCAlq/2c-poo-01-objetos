b5e6e32210fd733793f63ad02f25c46175586fcf
amanda-vitoria-nave <amanda.rodrigues@nave.org.br>
2026-06-05 11:06:44 -0300

    aAa

--- src/AtvMetodoFacil.js
+++ src/AtvMetodoFacil.js
@@ -20,3 +20,34 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+const personagem = {
+  nome: "Romeu Rômulo",
+  classe: "bardo",
+  nivel: 12,
+  equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro",
+  "capa de couro", "chapéu de pena"],
+  vida: 100,
+  magia: 100,
+  moeda: {
+    ouro: 20, prata: 43, bronze: 125
+  },
+  pocoes: {
+    cura: 7, magia: 5 
+  }, 
+  tocarMusica: function (musica, letra) {
+    console.log Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+uma música para trazer ânimo a seus corpos e esperança para seus corações.
+Apresento-lhes a música [musica].
+  
+'Em 1, 2, 3'
+   '...'
+[letra]
+  }
+
+}
+
+personagem.tocarMusica("Oceano", `voce desagua em mim, e eu, oceano`)
+
+
+export default personagem
+
--- src/Objeto.js
+++ src/Objeto.js
@@ -113,3 +113,4 @@ console.table(meuPapagaio.medicado)
 
 meuPapagaio.dormir()
 console.table(meuPapagaio)
+
