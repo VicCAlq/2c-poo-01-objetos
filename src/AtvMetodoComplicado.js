a29ccb8f7e9620f37b77b460cfe357293b8b3dbb
Ryan-Nave2026 <ryan.nave2026@gmail.com>
2026-06-06 19:35:14 +0000

    fix

--- src/AtvMetodoComplicado.js
+++ src/AtvMetodoComplicado.js
@@ -39,3 +39,69 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+const personagem = {
+    nome: 'Romeu Rômulo',
+    nivel: 12,
+    classe: 'bardo',
+    equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro", "capa de couro", "chapéu de pena"],
+    pontosDeVida: 30,
+    pontosDeMagia: 100,
+    moedas: {
+        ouro: 20,
+        prata: 43,
+        bronze: 125
+    }, 
+    pocoes: {
+        cura: 0,
+        magia: 5
+    },
+    tocarMusica: function(nome, letra){
+        console.log(
+        `Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+        uma música para trazer ânimo a seus corpos e esperança para seus corações.
+        Apresento-lhes a música [${nome}].
+
+        Em 1, 2, 3
+
+        ${letra}`) 
+    },
+    consumirPorcao: function(){
+
+        if(this.pontosDeVida < 100) {
+            this.pontosDeVida = 100;
+            
+            this.pocoes.cura -= 1
+        }
+
+    },
+    comprarPorcoes: function(QuantidadeDePorcoesCompradas){
+        const custoPorPocao = 15;
+        const custoTotal = custoPorPocao * quantidadeDePorcoesCompradas;
+
+        const totalEmBronze =
+            this.moedas.ouro * 100 +
+            this.moedas.prata * 10 +
+            this.moedas.bronze;
+
+        const custoTotalEmBronze = custoTotal * 10;
+
+        if (custoTotalEmBronze > totalEmBronze) {
+            console.log("Você não tem dinheiro o suficiente, meu caro. Quer fazer um empréstimo?");
+            return;
+        }
+        const restoEmBronze = totalEmBronze - custoTotalEmBronze;
+
+        this.moedas.ouro   = Math.floor(restoEmBronze / 100);
+        this.moedas.prata  = Math.floor((restoEmBronze % 100) / 10);
+        this.moedas.bronze = restoEmBronze % 10;
+
+        this.pocoes.cura += quantidadeDePorcoesCompradas;
+
+        console.log(`${quantidadeDePorcoesCompradas} poção(ões) comprada(s)! Poções de cura: ${this.pocoes.cura}`);
+    }
+}
+
+console.log(personagem)
+personagem.tocarMusica('Blue Bird', 'hishou taitara\n modoranai to itte\n mezashita no wa\n aoi aoi ano sora\n kanashimi wa mada oboerarezu\n setsunasa wa ima tsukami hajimetaanata\n e to daku kono kanjou mo\n ima kotoba ni kawatte iku\n michi naru sekai no\n yuumei kara mezamete\n kono hane wo hiroge tobitatsu\n hishou taitara\n modoranai to itte\n mezashita no wa\n shiroi shiroi ano kumo\n tsukinuketara\n mitsukaru to shitte\n furikiru hodo\n aoi aoi ano sora\n aoi aoi ano sora\n aoi aoi ano sora')
+personagem.consumirPorcao()
+export default personagem;
