0b93875274bf2f4809894df6518c669391cd0ceb
samuelerivaldo-png <samuel.erivaldo@nave.org.br>
2026-06-07 20:43:42 -0300

    commit 2

--- src/AtvMetodoFacil.js
+++ src/AtvMetodoFacil.js
@@ -24,6 +24,7 @@ const personagem = {
   nome: "Romeu Rômulo",
   classe: "bardo",
   nivel: 12,
+
   equipamento: [
     "arco",
     "violão",
@@ -33,30 +34,38 @@ const personagem = {
     "capa de couro",
     "chapéu de pena"
   ],
-  
-pontosDeVida: 100,
-pontosDeMagia: 100,
 
-moedas: {
+  pontosDeVida: 100,
+  pontosDeMagia: 100,
 
- ouro: 20,
- prata: 43,
- bronze: 125
+  moedas: {
+    ouro: 20,
+    prata: 43,
+    bronze: 125
+  },
 
-},
+  pocoes: {
+    cura: 7,
+    magia: 5
+  },
 
-pocoes: {
-  cura: 7,
-  magia: 5
+  tocarMusica: function(musica, letra) {
+    console.log(`
+Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+uma música para trazer ânimo a seus corpos e esperança para seus corações.
+Apresento-lhes a música ${musica}.
 
-},
+Em 1, 2, 3
+...
 
-tocarMusica: function(musica, letra) {
-   
-
-
+${letra}
+`)
+  }
 }
 
-}
+personagem.tocarMusica(
+  "Montagem Domer",
+  `Domer`
+)
 
 export default personagem
