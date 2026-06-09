d8ff5e58a130ac7eddd87791fceecf20ba9298fc
gustavoalberto-create <gustavo.alberto@nave.org.br>
2026-06-08 21:28:17 -0300

    pronto

--- src/AtvMetodoComplicado.js
+++ src/AtvMetodoComplicado.js
@@ -39,3 +39,65 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+const personagem = {
+  nome: "Romeu Rômulo",
+  classe: "bardo",
+  nivel: 12,
+  equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro", "capa de couro", "chapéu de pena"],
+  pontosDeVida: 30,
+  pontosDeMagia: 100,
+  moedas: {
+    ouro: 20,
+    prata: 43,
+    bronze: 125
+  },
+  pocoes: {
+    cura: 0, 
+    magia: 5
+  },
+  tocarMusica: function(musica, letra) {
+    console.log(`Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso\numa música para trazer ânimo a seus corpos e esperança para seus corações.\nApresento-lhes a música ${musica}.\n\nEm 1, 2, 3\n...\n${letra}`);
+  },
+  tomarPocaoCura: function() {
+    if (this.pocoes.cura > 0) {
+      this.pocoes.cura -= 1;
+      this.pontosDeVida = 100;
+      console.log("Poção consumida! A vida de Romeu Rômulo voltou para 100.");
+    } else {
+      console.log("Você não possui poções de cura.");
+    }
+  },
+  comprarPocaoCura: function(quantidade) {
+    
+    const custoEmPrata = quantidade * 15;
+    
+    const custoEmBronze = custoEmPrata * 10;
+    
+    
+    let carteiraEmBronze = this.moedas.bronze + (this.moedas.prata * 10) + (this.moedas.ouro * 100);
+
+   
+    if (carteiraEmBronze >= custoEmBronze) {
+      
+      carteiraEmBronze -= custoEmBronze;
+      
+      
+      this.moedas.ouro = Math.floor(carteiraEmBronze / 100);
+      carteiraEmBronze = carteiraEmBronze % 100; 
+      
+      this.moedas.prata = Math.floor(carteiraEmBronze / 10);
+      this.moedas.bronze = carteiraEmBronze % 10; 
+      
+   o
+      this.pocoes.cura += quantidade;
+      
+      console.log(`Compra concluída! Você adquiriu ${quantidade} poções de cura.`);
+    } else {
+      console.log("Você não tem dinheiro o suficiente, meu caro. Quer fazer um empréstimo?");
+    }
+  }
+};
+
+
+
+export default personagem;
