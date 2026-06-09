6957e75e9150007554c43ceb1e470b0a6115039b
Teteuzo <matheus.thiago@nave.org.br>
2026-06-08 23:04:58 -0300

    atv objeto, metodofacil, metodomedio concluidas

--- src/AtvMetodoMedio.js
+++ src/AtvMetodoMedio.js
@@ -16,3 +16,46 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+const personagem = {
+
+nome: "Romeu Rômulo",
+classe: "bardo",
+nivel: 12,
+equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro", "capa de couro", "chapéu de pena"],
+pontosDeVida: 30,
+pontosDeMagia: 100,
+moedas: 
+  {
+    ouro: 20,
+    prata: 43,
+    bronze: 125,
+  },
+pocoes: 
+  {
+    cura: 7,
+    magia: 5,
+  },
+
+
+tocarMusica: function(musica, letra) {
+  console.log (`Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+     uma música para trazer ânimo a seus corpos e esperança para seus corações.
+     Apresento-lhes a música ${musica}.
+     Em 1, 2, 3... 
+     ${letra}
+    `);
+    },
+  
+seCurar: function() {
+if(this.pontosDeVida == 100) {
+  console.log("foi venerado pelo publico e saiu ileso")
+}
+else{
+  this.pocoes.cura -= 1;
+  this.pontosDeVida += 70;
+  console.log(this.pontosDeVida);
+}
+
+}
+}
+export default personagem
