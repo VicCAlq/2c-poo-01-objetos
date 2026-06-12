129a90ff059abc8130232d45622c2804e6366760
Teteuzo <matheus.thiago@nave.org.br>
2026-06-12 11:09:15 -0300

    masi uma vez

--- src/AtvMetodoMedio.js
+++ src/AtvMetodoMedio.js
@@ -18,44 +18,41 @@
 
 const personagem = {
 
-nome: "Romeu Rômulo",
-classe: "bardo",
-nivel: 12,
-equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro", "capa de couro", "chapéu de pena"],
-pontosDeVida: 30,
-pontosDeMagia: 100,
-moedas: 
-  {
+  nome: "Romeu Rômulo",
+  classe: "bardo",
+  nivel: 12,
+  equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro", "capa de couro", "chapéu de pena"],
+  pontosDeVida: 30,
+  pontosDeMagia: 100,
+  moedas: {
     ouro: 20,
     prata: 43,
     bronze: 125,
   },
-pocoes: 
-  {
+  pocoes: {
     cura: 7,
     magia: 5,
   },
 
-
-tocarMusica: function(musica, letra) {
-  console.log (`Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
-     uma música para trazer ânimo a seus corpos e esperança para seus corações.
-     Apresento-lhes a música ${musica}.
-     Em 1, 2, 3... 
-     ${letra}
+  tocarMusica: function(musica, letra) {
+    console.log (`Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+      uma música para trazer ânimo a seus corpos e esperança para seus corações.
+      Apresento-lhes a música ${musica}.
+      Em 1, 2, 3... 
+      ${letra}
     `);
-    },
-  
-seCurar: function() {
-if(this.pontosDeVida == 100) {
-  console.log("foi venerado pelo publico e saiu ileso")
-}
-else{
-  this.pocoes.cura -= 1;
-  this.pontosDeVida += 70;
-  console.log(this.pontosDeVida);
+  },
+    
+  seCurar: function() {
+    if(this.pontosDeVida == 100) {
+      console.log("foi venerado pelo publico e saiu ileso")
+    }
+    else{
+      this.pocoes.cura -= 1;
+      this.pontosDeVida += 70;
+      console.log(this.pontosDeVida);
+    }
+  }
 }
 
-}
-}
 export default personagem
