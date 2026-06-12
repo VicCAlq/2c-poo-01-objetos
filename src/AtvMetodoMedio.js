57d88b6c813e4a561ee4dfcd94fa59e5d0b66e18
Thales Henry <thales.henry@nave.org.br>
2026-06-12 10:55:25 -0300

    Mudei

--- src/AtvMetodoMedio.js
+++ src/AtvMetodoMedio.js
@@ -1,27 +1,34 @@
 /*
-  * Exercício 3
-  *
-  * Copie o personagem do exercício 2, e mude os "pontosDeVida" dele 
-  * de "100" para "30".
-  * 
-  * Após cantar a música no exercício 2, a platéia não gostou nada e moeu
-  * o personagem na pancada, reduzindo seus "pontosDeVida".
-  *
-  * Crie agora um método onde o personagem pode consumir uma poção
-  * que ele possua para recuperar sua vida para o valor total.
-  *
-  * O método deve além de curar os pontos de vida, deve também remover uma 
-  * das poções de seu inventário.
-  *
-  * Lembre-se de exportar o objeto do personagem no final do código.
-  */
+ * Exercício 3
+ *
+ * Copie o personagem do exercício 2, e mude os "pontosDeVida" dele
+ * de "100" para "30".
+ *
+ * Após cantar a música no exercício 2, a platéia não gostou nada e moeu
+ * o personagem na pancada, reduzindo seus "pontosDeVida".
+ *
+ * Crie agora um método onde o personagem pode consumir uma poção
+ * que ele possua para recuperar sua vida para o valor total.
+ *
+ * O método deve além de curar os pontos de vida, deve também remover uma
+ * das poções de seu inventário.
+ *
+ * Lembre-se de exportar o objeto do personagem no final do código.
+ */
 const personagem = {
   nome: "Romeu Rômulo",
   classe: "bardo",
   nivel: 12,
-  equipamento:["arco", "violão", "gaita", "flechas de aço",
-  "botas de couro", "capa de couro", "chapéu de pena"],
-  pontosDeVida: 30,
+  equipamento: [
+    "arco",
+    "violão",
+    "gaita",
+    "flechas de aço",
+    "botas de couro",
+    "capa de couro",
+    "chapéu de pena",
+  ],
+  pontosDeVida: 100,
   pontosDeMagia: 100,
   moedas: {
     ouro: 20,
@@ -32,15 +39,19 @@ const personagem = {
     cura: 7,
     magia: 5,
   },
-  tocarMusica: function(musica, letra) {
+  tocarMusica: function (musica, letra) {
     console.log(`Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso uma música para trazer ânimo a seus corpos e esperança para seus corações. 
       Apresento-lhes a música ${musica}. 
       Em 1, 2, 3... 
-      ${letra}`)
+      ${letra}`);
   },
-  recuperarVida: function(){
-  this.pontosDeVida = 100
-  this.pocoes.cura = this.pocoes.cura - 1
-  }
-}
-export default personagem
+  recuperarVida: function () {
+    if (this.pocoes.cura > 0) {
+      this.pocoes.cura = this.pocoes.cura - 1;
+      this.pontosDeVida = 100;
+    } else {
+      console.log("Não ha poções de cura disponivel.");
+    }
+  },
+};
+export default personagem;
