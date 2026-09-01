3fa07a16c3b93a1f28a989e97e9ca8b613eeff99
Renato-Marques-Nave <renato.marques@nave.org.br>
2026-09-01 13:28:55 -0300

    perdoe-me

--- src/AtvMetodoMedio.js
+++ src/AtvMetodoMedio.js
@@ -15,36 +15,4 @@
   *
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
-
-const personagem = {
-  nome: "Romeu Rômulo",
-  classe: "bardo",
-  nivel: 12,
-  equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro", "capa de couro", "chapéu de pena"],
-  pontosDeVida: 30,
-  pontosDeMagia: 100,
-
-  moedas: {
-    ouro: 20,
-    prata: 43,
-    bronze: 125
-  },
-  
-  pocoes: {
-    cura: 7,
-    magia: 5
-  },
-
-  tocarMusica: function(musica, letra) {
-    console.log(`Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso uma música para trazer ânimo a seus corpos e esperança para seus corações. Apresento-lhes a música ${musica}.\n\nEm 1, 2, 3\n...\n${letra}`);
-  },
-
-  chamaODoc: function() {
-    if (this.pocoes.cura > 0) {
-      this.pocoes.cura -= 1;
-      this.pontosDeVida = 100;
-    }
-  }
-}
-
-export default personagem
+pardon
